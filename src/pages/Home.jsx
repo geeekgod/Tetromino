1620e46460fa1b7d91f4d6369ee4379295e4ac42
Rishabh Singh <ris8645208@gmail.com>
2021-09-25 14:53:34 +0530

    chore: add hooks for scroll behavior

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Home.css';
 import starsImg from '../img/stars.png'
 import moonImg from '../img/moon.png'
@@ -7,30 +7,20 @@ import mountainFrontImg from '../img/mountains_front.png'
 
 const Home = () => {
 
-    let stars = document.getElementById('stars')
-    let moon = document.getElementById('moon')
-    let mountains_behind = document.getElementById('mountains_behind')
-    let text = document.getElementById('text')
-    let btn = document.getElementById('btn')
-    let mountains_front = document.getElementById('mountains_front')
-    let header = document.querySelector('header');
-    window.addEventListener('scroll', function () {
-        let value = window.scrollY;
-        if (stars && moon && mountains_front && mountains_behind && text && btn) {
-            stars.style.left = value * .25 + 'px';
-            moon.style.top = value * 1.05 + 'px';
-            mountains_behind.style.top = value * 0.5 + 'px';
-            mountains_front.style.top = value * 0 + 'px';
-            text.style.marginRight = value * 4 + 'px';
-            text.style.marginTop = value * 1.5 + 'px';
-            btn.style.marginTop = value * 1.5 + 'px';
-            header.style.top = value * 0.5 + 'px';
-        }
-    })
+    const [scrollValue, setScrollValue] = useState();
+    let value = 0;
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            value = window.scrollY;
+            setScrollValue(value);
+            console.log(scrollValue);
+        })
+    }, [value])
 
     return (
-        <div>
-            <header>
+        <>
+            <header style={{ top: scrollValue * 0.5 + 'px' }}>
                 <a href="#home" class="logo">tetromino</a>
                 <ul>
                     <li><a href="#home" class="active">HOME</a></li>
@@ -40,12 +30,12 @@ const Home = () => {
                 </ul>
             </header>
             <section class="images">
-                <img src={starsImg} alt="" id="stars" />
-                <img src={moonImg} alt="" id="moon" />
-                <img src={mountainBehindImg} alt="" id="mountains_behind" />
-                <h2 id="text">Tetromino</h2>
-                <a href="#" id="btn">EXPLORE</a>
-                <img src={mountainFrontImg} alt="" id="mountains_front" />
+                <img src={starsImg} alt="" id="stars" style={{ left: scrollValue * .25 + 'px' }} />
+                <img src={moonImg} alt="" id="moon" style={{ top: scrollValue * 1.05 + 'px' }} />
+                <img src={mountainBehindImg} alt="" id="mountains_behind" style={{ top: scrollValue * 0.5 + 'px' }} />
+                <h2 id="text" style={{ marginRight: scrollValue * 4 + 'px', marginTop: scrollValue * 1.5 + 'px' }}>Tetromino</h2>
+                <a href="#" id="btn" style={{ marginTop: scrollValue * 1.5 + 'px' }}>EXPLORE</a>
+                <img src={mountainFrontImg} alt="" id="mountains_front" style={{ top: scrollValue * 0 + 'px' }} />
             </section>
             <div class="sec">
                 <h2>hello</h2>
@@ -59,8 +49,8 @@ const Home = () => {
 
                 </p>
             </div>
-        </div>
+        </>
     )
 }
 
-export default Home;
+export default React.memo(Home);
