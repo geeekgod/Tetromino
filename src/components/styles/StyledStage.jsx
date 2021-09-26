4426eb6edc5fb2efd2f0d05a61bad2a18b90a1f8
Rishabh Singh <ris8645208@gmail.com>
2021-09-26 20:57:19 +0530

    style: update Home and play game styles

--- src/components/styles/StyledStage.jsx
+++ src/components/styles/StyledStage.jsx
@@ -8,7 +8,7 @@ export const StyledStage = styled.div`
   );
   grid-template-columns: repeat(${props => props.width}, 1fr);
   grid-gap: 1px;
-  border: 2px solid #333;
+  border: 2px solid rgba(277,277,277,0.5);
   width: 100%;
   max-width: 25vw;
   background: #111;
